99863214019ab4c60f04bf84bb60a3a8b42d80c2
Felix Hutnyk <felix.hutnyk@gmail.com>
2022-10-15 17:42:25 -0400

    added login system

--- index.php
+++ index.php
@@ -1,27 +1,32 @@
 <?php
 
-$scan_directory="docs";
+require 'config.php';
 
+if ($authentication_enabled) {
+    if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] === false) {
+        header("location: login.php");
+        exit;
+    }
+}
 
-function getAllContentOfLocation($loc) {   
+function getAllContentOfLocation($loc) {
     $scandir = scandir($loc);
-
-    $scandir = array_filter($scandir, function($element) {
+    $scandir = array_filter($scandir, function ($element) {
         return !preg_match('/^\./', $element);
     });
 
-    if(empty($scandir)) echo '<a style="color:red">Empty Dir</a>';
+    if (empty($scandir)) echo '<a style="color:red">Empty Dir</a>';
 
-    foreach($scandir as $file) {
+    foreach ($scandir as $file) {
         $baseLink = $loc . DIRECTORY_SEPARATOR . $file;
 
         echo '<ul>';
-        if(is_dir($baseLink)) {
-            echo '<li class="collapse-dir"><a class="DIR" style="font-weight:bold;">'.$file.'</a>';
+        if (is_dir($baseLink)) {
+            echo '<li class="collapse-dir"><a class="DIR" style="font-weight:bold;">' . $file . '</a>';
             getAllContentOfLocation($baseLink);
             echo '</li>';
         } else {
-            echo '<li class="NORM"><a class="NORM" href="'.$baseLink.'">'.$file.'</a></li>';
+            echo '<li class="NORM"><a class="NORM" href="' . $baseLink . '">' . $file . '</a></li>';
         }
         echo '</ul>';
     }
@@ -31,15 +36,16 @@ function getAllContentOfLocation($loc) {
 
 <!DOCTYPE html>
 <html>
+
 <head>
-    <title>Notes Viewer</title>
+    <title></title>
     <link rel="stylesheet" href="./assets/md.css">
     <link rel="stylesheet" href="./assets/style.css">
     <link rel="icon" type="image/x-icon" href="/assets/favicon.ico">
 </head>
 
 <body>
-    <div class="content">        
+    <div class="content">
 
         <div id="preview" class="preview md">
         </div>
---
+++ login.php
@@ -0,0 +1,64 @@
+<?php
+
+require 'config.php';
+
+if ($authentication_enabled) {
+    if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
+        header("location: index.php");
+        exit;
+    }
+}
+
+$password = "";
+$password_err = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty(trim($_POST["password"]))) {
+        $password_err = "Please enter your password.";
+    } else {
+        $password = trim($_POST["password"]);
+    }
+
+    if ($password == $authentication_password) {
+        $_SESSION["loggedin"] = true;
+        header("location: index.php");
+    } else {
+        $password_err = "Incorrect password.";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - Notes Viewer</title>
+    <link rel="stylesheet" href="./assets/login.css">
+</head>
+
+<body>
+    <div class="box">
+
+        <div class="form">
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                <h2>Authentication</h2>
+                <div class="inputBox">
+                    <input name="password" type="password" required="required">
+                    <span>Password</span>
+                    <i></i>
+                </div>
+
+                <br>
+                <?php echo "<h2>".$password_err."</h2>"; ?>
+                <br>
+                <input type="submit" value="Login" class="c">
+            </form>
+        </div>
+    </div>
+
+</body>
+
+</html>
